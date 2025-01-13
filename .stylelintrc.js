c5d5cecca9211d5c77aef783a57ad1a636278156
luochun <2591451512@qq.com>
2025-01-13 19:13:28 +0800

    style: init

---
+++ .stylelintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+	// 从标准配置中继承规则
+	extends: ['stylelint-config-standard'],
+
+	// 规则配置
+	rules: {
+		'no-descending-specificity': null,
+		'plugin/declaration-block-no-ignored-properties': true,
+		'function-name-case': null,
+	},
+	plugins: ['stylelint-declaration-block-no-ignored-properties'],
+
+	// 忽略检查的文件或文件夹
+	ignoreFiles: ['node_modules/**/*', 'build/**/*', 'dist/**/*'],
+}
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': '/src',
+			'@/components': '/src/components',
+			'@/utils': '/src/utils',
+		},
+	},
+	server: {
+		port: 3001,
+		open: true,
+	},
 })
